d5ea3286d414faad643a61f8c610764b71ff32dc
yangmiemie1 <11094759234@qq.com>
2019-08-06 16:41:05 +0800

    city router

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,18 +1,10 @@
 <template>
     <div>
-<<<<<<< HEAD
-        <home-header></home-header>
-        <home-swiper></home-swiper>
-        <home-icons></home-icons>
-        <home-recommend></home-recommend>
-        <home-weekend></home-weekend>
-=======
         <home-header :city="city"></home-header>
         <home-swiper :list="swiperList"></home-swiper>
         <home-icons :list="iconList"></home-icons>
         <home-recommend :list="recommendList"></home-recommend>
         <home-weekend :list="weekendList"></home-weekend>
->>>>>>> index-ajax
     </div>
 </template>
 
@@ -22,11 +14,8 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
-<<<<<<< HEAD
-=======
 import axios from 'axios'
 
->>>>>>> index-ajax
 export default {
   name: 'Home',
   components: {
@@ -35,8 +24,6 @@ export default {
     HomeIcons,
     HomeRecommend,
     HomeWeekend
-<<<<<<< HEAD
-=======
   },
   data () {
     return {
@@ -67,7 +54,6 @@ export default {
   },
   mounted () {
     this.getHomeInfo()
->>>>>>> index-ajax
   }
 }
 </script>
--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-    <template>
-    <div>
-        <div class="title">热销推荐</div>
-        <ul>
-            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
-=======
 <template>
     <div>
         <div class="title">热销推荐</div>
         <ul>
             <li class="item border-bottom" v-for="item of list" :key="item.id">
->>>>>>> index-ajax
                 <img class="item-img" :src="item.imgUrl" alt="">
                 <div class="item-info">
                     <p class="item-title">{{item.title}}</p>
@@ -25,48 +17,11 @@
 <script>
 export default {
   name: 'HomeRecommend',
-<<<<<<< HEAD
-  data () {
-    return {
-      recommendList: [
-        {
-          id: '0001',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg',
-          title: '北京欢乐谷',
-          desc: '亚洲唯一飞行式过山车等你来挑战'
-        },
-        {
-          id: '0002',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg',
-          title: '故宫',
-          desc: '亚洲唯一飞行式过山车世界五大宫之首，穿越与您近在咫尺等你来挑战'
-        },
-        {
-          id: '0003',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1708/2b/2b3b94de99c0a425a3.img.jpg_200x200_2458ffb2.jpg',
-          title: '八达岭长城',
-          desc: '不到长城非好汉'
-        },
-        {
-          id: '0004',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1602/68/68aa05adb5315f9990.water.jpg_200x200_ce3d2658.jpg',
-          title: '北京海洋馆',
-          desc: '奇幻海洋之旅，在帝都感受大海的魅力'
-        },
-        {
-          id: '0005',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1609/7a/7ae8ee7831836095a3.water.jpg_200x200_36727f69.jpg',
-          title: '金海湖风景区',
-          desc: '峰峦叠翠，野卉生香，云影波光，美景如画'
-        }
-      ]
-=======
   props: {
     list: Array
   },
   data () {
     return {
->>>>>>> index-ajax
     }
   }
 }
