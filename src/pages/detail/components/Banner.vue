5ecd7af162b46088b05d0ea57967ff56e0b2cd55
yangmiemie1 <11094759234@qq.com>
2019-08-08 17:41:48 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_r_800x800_087730c7.jpg" alt="">
+            <div class="banner-info">
+                <div class="banner-title">
+                    北京欢乐谷(AAAA景区)
+                </div>
+                <div class="banner-number">
+                    <span class="iconfont banner-icon">&#xe637;</span>39
+                </div>
+            </div>
+        </div>
+        <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryColose"></common-gallary>
+    </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: [
+        'http://img1.qunarzz.com/sight/p0/1508/a5/4003f9dd7bebf61eccbf64046e26d487.water.jpg_r_800x800_4701d58f.jpg', 'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_r_800x800_087730c7.jpg'
+      ]
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryColose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #ffffff
+      background-image: linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,0.8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        margin-top: .14rem
+        padding: 0 .4rem
+        line-height: .32rem
+        height: .32rem
+        border-radius: .4rem
+        background: rgba(0, 0, 0, 0.8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+
+</style>
